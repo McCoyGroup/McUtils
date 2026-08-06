af5dfaff8ccc12f982eea0d75741c705827642c3
b3m2a1 <b3m2a1@gmail.com>
2026-08-06 13:25:08 -0500

    Extend tree flattening to support jump tables

--- ci/tests/ScaffoldingTests.py
+++ ci/tests/ScaffoldingTests.py
@@ -53,7 +53,7 @@ class ScaffoldingTests(TestCase):
         }
         self.assertFalse(schema.validate(data, throw=False))
 
-    @debugTest
+    @validationTest
     def test_TreeFlattening(self):
         data = {
             'file': 'test.txt',
@@ -78,6 +78,55 @@ class ScaffoldingTests(TestCase):
         buf.seek(0)
         rev2 = read_flat_tree(buf)
         print(rev2)
+
+    @debugTest
+    def test_TreeArchiveJumps(self):
+        data = {
+            'file': 'test.txt',
+            'filesystem': {'file': [['a'], ['b', 'c']]},
+            'coords': 123,
+            'initial': {
+                'coords': np.random.rand(5, 3)
+            },
+            'final': {
+                'coords': np.random.rand(1, 2)
+            },
+            'a': {'thing': {'b': 1, 'c': 2, "_type": "A", 'other': None}, 'other': None},
+            'c': {'thing': None, 'other': 9.1},
+            'g': {"c": np.full(5, None)}
+        }
+
+        archive = NumpyTreeArchive.from_tree(data)
+
+        buf = io.BytesIO()
+        archive.save(buf)
+        buf.seek(0)
+
+        new_archive = NumpyTreeArchive.load(buf)
+        print(new_archive)
+
+        print(new_archive['a'])
+
+        big_data = {
+            f'entry_{i}':{
+                'state_index':i,
+                'energy':np.random.rand(1, 2),
+                'coords':np.random.rand(1000, 3)
+            }
+            for i in range(1000)
+        }
+
+        archive = NumpyTreeArchive.from_tree(big_data)
+
+        buf = io.BytesIO()
+        archive.save(buf)
+        buf.seek(0)
+
+        new_archive = NumpyTreeArchive.load(buf)
+        print(new_archive)
+
+        print(new_archive['entry_100'])
+
 
     #region Checkpointing
     @validationTest
